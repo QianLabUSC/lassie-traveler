6ac176ba397d5a14c5de4548aff71a877a4a7aec
Yifeng Zhang <yifengz@usc.edu>
2025-06-11 11:31:00 -0700

    clean up files

--- dependencies/build/traveler_high_controller/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/eeb16/Dropbox/USC/Research/Project/lassie-traveler/dependencies/traveler_controller
-# Build directory: /home/eeb16/Dropbox/USC/Research/Project/lassie-traveler/dependencies/build/traveler_high_controller
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- dependencies/build/traveler_high_controller/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/eeb16/Dropbox/USC/Research/Project/lassie-traveler/dependencies/install/traveler_msgs/share/traveler_msgs/package.sh"
